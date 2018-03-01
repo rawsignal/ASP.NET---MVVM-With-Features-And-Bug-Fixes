c0f297a81c461b67de1e96854c42c42e9fa806e0
Logan Rossignol <logan_rossignol@valpak.com>
2018-03-01 12:07:22 -0500

    Initial Commit

---
+++ MVVMWebAppTemplate/MVVMWebAppTemplate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVVMWebAppTemplate.MvcApplication" Language="C#" %>
---
+++ MVVMWebAppTemplate/MVVMWebAppTemplate/Scripts/app/test.viewmodel.js
@@ -0,0 +1,20 @@
+﻿function TestViewModel(app, dataModel) {
+    var self = this;
+
+    self.setLoadingOn = function () {
+        isLoading(true);
+    };
+
+    self.setLoadingOff = function () {
+        isLoading(false);
+    };
+
+
+    return self;
+}
+
+app.addViewModel({
+    name: "Test",
+    bindingMemberName: "test",
+    factory: TestViewModel
+});
